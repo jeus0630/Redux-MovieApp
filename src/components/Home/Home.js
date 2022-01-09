f543fef3a7ff8b3eeea39329caaa24a7da449ef2
jewoo <jeus0630@gmail.com>
2022-01-09 12:12:25 -0500

    toolkit & main design

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,23 +2,24 @@ import React, {useEffect} from 'react';
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/apis/movieApi";
 import {APIKey} from "../../common/apis/movieApiKey";
-import {addMovies} from "../../features/movies/movieSlice";
-import {useDispatch, useSelector} from "react-redux";
-
+import {useDispatch} from "react-redux";
+import {movieReducer} from "../../features/movie/movie";
 const Home = () => {
     const dispatch = useDispatch();
-
     useEffect(()=>{
         const movieText = "Harry";
 
-        const fetchMovies = async () =>{
-            const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`).catch((err)=>{
-                console.log(err)});
-            dispatch(addMovies(response.data));
+        const fetchMovies = async () => {
+            const result = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`).
+            catch(err=>{
+                console.log(err);
+            });
+            dispatch(movieReducer(result.data));
         }
 
         fetchMovies();
     },[]);
+
     return (
         <>
             <div className="banner-img"></div>
--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,14 +1,20 @@
-import React from 'react';
 import {useSelector} from "react-redux";
-import {getAllMovies} from "../../features/movies/movieSlice";
+import {getAllMovies} from "../../features/movie/movie";
+import MovieCard from "../MovieCard/MovieCard";
+import "./MoveListing.scss";
 
 const MovieListing = () => {
+
     const movies = useSelector(getAllMovies);
+    const renderMovies = movies.Search?.map((el,idx)=><MovieCard key={idx} data={el}></MovieCard>)
     return (
-        <div>
-            {
-                movies.Search?.map((el,idx)=><div key={idx}>{el.Title}</div>)
-            }
+        <div className="movie-wrapper">
+            <div className="movie-list">
+                <h2>Movies</h2>
+                <div className="movie-container">
+                    {renderMovies}
+                </div>
+            </div>
         </div>
     );
 };
